4b500f79aa9764e52a852b7725068ea4753a5b88
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2020-01-08 02:54:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,9 +9,8 @@ class Artist
     song.artist = self
   end
   
-  def songs
-    Song.all.select {|song| song.artist == self}
-    
+  def add_song(song)
+    song.artist = self
   end
   
   def add_song_by_name(name, genre)
